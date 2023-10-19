152ecc561f2ceeb82d5d0970a3d23cc12d472736
Lucas Miotto <latmiotto@gmail.com>
2023-10-19 11:50:01 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -22,13 +22,24 @@ export default async function Home() {
         sizes="100vw"
         alt="Até 55% de desconto esse mês!"
       />
+
       <div className="mt-8 p-5">
         <Categories />
       </div>
 
       <div className="mt-8">
+        <p className="mb-2 pl-5 font-bold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <Image
+        src="/banner-home-02.png"
+        height={0}
+        width={0}
+        className="h-auto w-full p-5"
+        sizes="100vw"
+        alt="Até 55% de desconto em Mouses!"
+      />
     </div>
   );
 }
